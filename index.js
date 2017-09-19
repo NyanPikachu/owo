8f2323b94f47c4e8dd5451365d67fec314e72c01
Andreas <apap04@otakoapp.com>
2017-09-18 23:19:10 -0400

    owo

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ bot.on('message', message => {
 
     if (message.content.startsWith(prefix + "help")) {
         message.reply(`sent help.`)
-        message.member.send("helpo here\n`owo^kick` Kicks a user.\n`^ban` Bans a user.\n`^eval` Can eval JS code. Only used by owner of bot and NOT by anyone else.\n`^contact` DMs contact info to you about the owner(s) of the bot.\n`^status` Shows status of bot.")
+        message.member.send("helpo here\n`owo^kick` Kicks a user.\n`owo^ban` Bans a user.\n`owo^eval` Can eval JS code. Only used by owner of bot and NOT by anyone else.\n`owo^contact` DMs contact info to you about the owner(s) of the bot.\n`owo^status` Shows status of bot.")
     }
 
     const args = message.content.split(" ").slice(1);
