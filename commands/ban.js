297c02f5e18548c3fdf422eb9c6bd0680db3a648
Andreas <apap04@otakoapp.com>
2017-09-21 18:08:27 -0400

    lol

--- commands/ban.js
+++ commands/ban.js
@@ -2,7 +2,7 @@ const prefix = "owo^"
 
 exports.run = (client, message, [mention, ...reason]) => {
     if (message.content.startsWith(prefix + 'ban')) {
-        if (!message.member.roles.some(r => ["Administrator", "Moderator", "Mod", "Admin", "Owner", "Co-Owner", "Modeh", "my love"].includes(r.name))) {
+        if (!message.member.roles.some(r => ["Administrator", "Admin", "Owner", "Co-Owner", "my love"].includes(r.name))) {
             return message.reply("You can't ban.")
         }
         if (message.mentions.users.size === 0) {
