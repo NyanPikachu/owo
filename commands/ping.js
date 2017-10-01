6346b24c16a2136ef0a89daf66923630d022cc67
Andreas <apap04@otakoapp.com>
2017-10-01 12:33:04 -0400

    'fixed' ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 const prefix = "owo^"
 
 exports.run = (client, message, args) => {
-    if (/^!ping/i.test(message.content)) {
+    if (message.content.startsWith(prefix + 'ping')) {
         message.channel.send('Pinging..').then(sent => {
             sent.edit(`**poNG!**\nmy latency is \`${sent.createdTimestamp - message.createdTimestamp}\`ms. - api latency is \`${Math.round(client.ping)}\`ms`);
         });
