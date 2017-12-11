9121a6eebe761d3967d6aa0be9d5674d8b691aa8
apap04 <apap04@otakoapp.com>
2017-12-10 22:13:21 -0500

    Bot won't reply, it'll send.

---
+++ commands/kiss.js
@@ -0,0 +1,10 @@
+const prefix = "owo^"
+
+exports.run = (client, message, [mention]) => {
+    if (message.content.startsWith(prefix + 'kiss')) {
+        if (message.mentions.users.size === 0) {
+            return message.channel.send("kiss who :>");
+        }
+        message.channel.send(`${member.user.username} has been kissed!`)
+    }
+}
