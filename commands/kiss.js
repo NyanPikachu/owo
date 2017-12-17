303d1a4e427c5664aef57561fe7fac1625a4f1a1
Andreas <apap04@otakoapp.com>
2017-12-16 20:19:57 -0500

    Will write soon owo

--- commands/kiss.js
+++ commands/kiss.js
@@ -13,6 +13,6 @@ exports.run = (client, message, [mention]) => {
         if (message.mentions.users.size === 0) {
             return message.channel.send("kiss who :>");
         }
-        message.channel.send(`${member.user.username} has been kissed!`)
+        message.reply(`kissed .`)
     }
 }
