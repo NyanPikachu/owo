3a9fc2624257baf1cb9f4f719abdeeea073b8770
Andreas P <apap04@otakoapp.com>
2017-12-24 21:12:55 -0500

    Added sum joke commands
    Fake discrim changer, succ command ;)
    
    Signed-off-by: Andreas P. <apap04@otakoapp.com>

---
+++ commands/discrim.js
@@ -0,0 +1,15 @@
+/**
+ * File name: discrim.js
+ * Description: change ur discrim (not really)
+ * Authors: Andreas P. <apap04@otakoapp.com>
+ * Licensed under Apache 2.0.
+ */
+
+const prefix = "owo^"
+
+exports.run = (client, message, args) => {
+    if (message.content.startsWith(prefix + 'discrim')) {
+        const discrim = args.join(" ");
+        message.reply("Your discriminator has been changed to " + discrim)
+    }
+}
---
+++ commands/succ.js
@@ -0,0 +1,17 @@
+/**
+ * File name: succ.js
+ * Description: SUCC PEOPLE OFF YES :^))))))))))))))))
+ * Authors: Andreas P. <apap04@otakoapp.com>
+ * Licensed under Apache 2.0.
+ */
+
+const prefix = "owo^"
+
+exports.run = (client, message, [mention]) => {
+    if (message.content.startsWith(prefix + 'succ')) {
+        if (message.mentions.users.size === 0) {
+            return message.channel.send("sUCC wHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO tAsTy :^)");
+        }
+        message.channel.send(`${message.author} SUCCE ${mention}!11!!1!!!1`)
+    }
+}
