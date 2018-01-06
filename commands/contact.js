7042665f65faa8c599f49a7e9d6702b2cd2d84c1
Andreas P <apap04@otakoapp.com>
2018-01-05 21:21:40 -0500

    dx ok:

--- commands/contact.js
+++ commands/contact.js
@@ -10,5 +10,5 @@ const prefix = "owo^"
 exports.run = (client, message, args) => {
     if (message.content.startsWith(prefix + 'contact')) 
     message.reply("sent contact info.")
-    message.member.send("Email: apap04@otakoapp.com\nDiscord: apap04#2897\nDiscord Invite Link: https://discord.gg/e37B8QQ\nIf you have any concerns about anything, feel free to message me. It would be also be appreciated if you report any leaks too. :)")
+    message.member.send("Email: apap04@otakoapp.com\nDiscord: apap04#8144\nDiscord Invite Link: https://discord.gg/e37B8QQ\nIf you have any concerns about anything, feel free to message me. It would be also be appreciated if you report any leaks too. :)")
 }
