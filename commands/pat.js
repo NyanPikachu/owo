cae2c4dc488377a0f995328594f18c65d0d5957b
Andreas <17275723+apap04@users.noreply.github.com>
2018-01-06 11:50:39 -0500

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -1,7 +1,7 @@
 /**
- * File name: kiss.js
- * Description: Kissing people ;)
- * Authors: Andreas P. <apap04@otakoapp.com>
+ * File name: pat.js
+ * Description:
+ * Authors: Andreas P. <apap04@otakoapp.com>, YazawaNic0 (https://github.com/YazawaNic0)
  * Licensed under Apache 2.0.
  */
 
@@ -12,6 +12,6 @@ exports.run = (client, message, [mention]) => {
         if (message.mentions.users.size === 0) {
             return message.channel.send("pat who? :V");
         }
-        message.channel.send(`${message.author} gave a pat to ${mention}. https://media1.tenor.com/images/bf646b7164b76efe82502993ee530c78/tenor.gif`)
+        message.channel.send(`${message.author} gave a pat to ${mention}.\nhttps://media1.tenor.com/images/bf646b7164b76efe82502993ee530c78/tenor.gif`)
     }
 }
