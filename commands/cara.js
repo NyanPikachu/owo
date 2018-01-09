63105e840ec7cc59f9faf66daca61488720808d4
Andreas P <apap04@otakoapp.com>
2018-01-08 21:20:43 -0500

    cawa
    owo owo wo owo wo oowo                 ooooooooooooooooooooooooooowow

---
+++ commands/cara.js
@@ -0,0 +1,14 @@
+/**
+ * File name: cara.js
+ * Description: cara uwu
+ * Authors: Andreas P. <apap04@otakoapp.com>
+ * Licensed under Apache 2.0.
+ */
+
+const prefix = "owo^"
+
+exports.run = (client, message) => {
+    if (message.content.startsWith(prefix + 'avatar')) {
+        message.reply("cara uwu");
+    }
+}
